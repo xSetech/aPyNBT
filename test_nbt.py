616b9b3c0004eb9ba1b70928c71efeffb9b1aa29
Seth Junot <xsetech@gmail.com>
2019-11-29 14:40:13 -0800

    Test serialization and comparison with original NBT data

--- test_nbt.py
+++ test_nbt.py
@@ -2,6 +2,8 @@
 """ Tests for nbt.py
 """
 
+import gzip
+
 import nbt
 
 
@@ -9,5 +11,28 @@ def test_deserialize_reference():
     """ Deserialization of the reference files should not fail
     """
     nbt.deserialize_file("level.dat.gz")
+
+
+def test_reserialize_reference():
+    """ Deserialize a file and then reserialize the data
+    """
+    tree = nbt.deserialize_file("level.dat.gz")
+    data = nbt.serialize(tree)
+
+
+def test_reserialize_reference_compared():
+    """
+    Same as test_reserialize_reference(), but compare the original uncompressed
+        bytes to the output of the serializer. There should be no difference.
+    """
+    with open("level.dat.gz", "rb") as compressed_nbt_file:
+        compressed_data = compressed_nbt_file.read()
+
+    # These are the bytes that the Minecraft client/server created.
+    original_data = gzip.decompress(compressed_data)
+    tree = nbt.deserialize(original_data)
+    data = nbt.serialize(tree)
+
+    assert data == original_data
 
 
