1f4c0e359153e1a54df92da720f2a8b8afa97449
Seth Junot <xsetech@gmail.com>
2019-12-09 20:36:44 -0800

    Added a directory for data blobs used in testing
    I've on hand a few saves and generated a few more for testing. These
    will not be kept in git because of their large size.

--- test_nbt.py
+++ test_nbt.py
@@ -12,13 +12,13 @@ import nbt
 def test_deserialize_reference():
     """ Deserialization of the reference files should not fail
     """
-    nbt.deserialize_file("level.dat.gz")
+    nbt.deserialize_file("test_data/level.dat.gz")
 
 
 def test_reserialize_reference():
     """ Deserialize a file and then reserialize the data
     """
-    tree = nbt.deserialize_file("level.dat.gz")
+    tree = nbt.deserialize_file("test_data/level.dat.gz")
     data = nbt.serialize(tree)
 
 
@@ -27,7 +27,7 @@ def test_reserialize_reference_compared():
     Same as test_reserialize_reference(), but compare the original uncompressed
         bytes to the output of the serializer. There should be no difference.
     """
-    with open("level.dat.gz", "rb") as compressed_nbt_file:
+    with open("test_data/level.dat.gz", "rb") as compressed_nbt_file:
         compressed_data = compressed_nbt_file.read()
 
     # These are the bytes that the Minecraft client/server created.
