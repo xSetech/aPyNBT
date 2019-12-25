059684f745564afc664963b312415a3752112fba
Seth Junot <xsetech@gmail.com>
2019-12-25 13:03:32 -0800

    Minor refactor of region.py and removal of chunk.py
    This project is focused on deserialization and serialization of
    Minecraft binary formats. That doesn't include higher-level
    constructions like Chunks, Entities, Players, etc.

--- chunk.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-""" Chunk Serializer and Deserializer
-
-https://minecraft.gamepedia.com/Chunk_format
-"""
-
-class Chunk:
-    pass
--- region.py
+++ region.py
@@ -6,26 +6,32 @@ https://minecraft.gamepedia.com/Anvil_file_format
 """
 
 from collections import defaultdict
-# import datetime
+from datetime import datetime
+from enum import IntEnum
 import gzip
 import re
 from struct import unpack
-from typing import List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple
 import zlib
 
-from chunk import Chunk
 import nbt
 
 re_coords_from_filename = re.compile(r"r\.([-0-9]+)\.([-0-9]+)\.mc[ar]")
+
 
 def coords_from_filename(filename: str, rgx=re_coords_from_filename) -> Tuple[int, int]:
     x, z = rgx.findall(filename)[0]
     return int(x), int(z)
 
 
+class Compression(IntEnum):
+    GZIP = 1
+    ZLIB = 2
+
+
 class Region:
 
-    __slots__ = ("x", "z", "chunks")
+    __slots__ = ("x", "z", "chunks", "timestamps", "compression")
 
     def __init__(self, region_data: memoryview, basename: str = None, x: int = None, z: int = None):
         """ Instantiate a McRegion
@@ -51,7 +57,9 @@ class Region:
         # region. In other words, the chunk's actual coordinates don't matter
         # here. For example, a chunk with coordinate (30, -1) cooresponds to
         # Region(x=0, z=-1).chunks[30][31].
-        self.chunks: Dict[int, Dict[int, Optional[Chunk]]] = defaultdict(lambda: defaultdict(lambda: None))
+        self.chunks: Dict[int, Dict[int, Optional[List[nbt.Tag]]]] = defaultdict(lambda: defaultdict(lambda: None))
+        self.timestamps: Dict[int, Dict[int, Optional[datetime]]] = defaultdict(lambda: defaultdict(lambda: None))
+        self.compression: Dict[int, Dict[int, Optional[int]]] = defaultdict(lambda: defaultdict(lambda: None))
 
         if basename is not None:
             self.x, self.z = coords_from_filename(basename)
@@ -60,51 +68,62 @@ class Region:
             self.z = z
 
         if region_data is not None:
-            self.deserialize(region_data, x, z)
+            self.deserialize(region_data)
 
     def deserialize_chunk(self, region_data: memoryview, x: int, z: int):
-        offset_offset = (32 * z) + (4 * x)
-        timestamp_offset = offset_offset + (4 * 1024)
+        """ Deserialize a chunk at offset coordinate (x, z)
 
-        # offset (3B) and sector count (1B, ignored)
-        offset_bytes = region_data[offset_offset:offset_offset + 3]
+        This method sets these attributes:
+            self.chunks (nbt trees)
+            self.timestamps (as datetime instances)
+            self.compression (an enum)
+
+        Chunk sector sizes are computed during serialization.
+        """
+        metadata_offset = (32 * z) + (4 * x)
+
+        # chunk data offset (3B) and sector count (1B)
+        offset_bytes = region_data[metadata_offset:metadata_offset + 3]
         offset = int.from_bytes(offset_bytes, byteorder='big', signed=False)
-        sectors = region_data[offset_offset + 3:offset_offset + 4][0]
+        sectors = region_data[metadata_offset + 3:metadata_offset + 4][0]
 
         if offset == 0 and sectors == 0:
             return  # ungenerated chunk
 
         # timestamp (4B)
         #   What timezone?... Also, 2038 problem...
-        #
-        # timestamp_int = unpack("!I", region_data[timestamp_offset:timestamp_offset + 4])[0]
-        # timestamp = datetime.datetime.fromtimestamp(timestamp_int)
-        chunk_offset = 4 * 1024 * offset  # from start of file, according to the docs
-        chunk_size_bytes = region_data[chunk_offset:chunk_offset + 4]
+        timestamp_offset = metadata_offset + 4096  # constant 4KiB offset
+        timestamp = unpack("!I", region_data[timestamp_offset:timestamp_offset + 4])[0]
+        chunk_last_update = datetime.fromtimestamp(timestamp)
+
+        # Chunk data (4B size, 1B compression, nB compressed NBT data)
+        chunk_offset: int = 4 * 1024 * offset  # from start of file, according to the docs
+        chunk_size_bytes: memoryview = region_data[chunk_offset:chunk_offset + 4]
         chunk_size: int = unpack("!I", chunk_size_bytes)[0]
-        compression: int = region_data[chunk_offset + 4:chunk_offset + 5][0]
+        chunk_compression: Compression = Compression(region_data[chunk_offset + 4:chunk_offset + 5][0])
 
         # Decompression and deserialization
-        decompressed_chunk_data: bytes = None
-        if compression == 1:
-            decompressed_chunk_data = gzip.decompress(region_data[chunk_offset + 5:chunk_offset + 5 + chunk_size])
-        elif compression == 2:
-            decompressed_chunk_data = zlib.decompress(region_data[chunk_offset + 5:chunk_offset + 5 + chunk_size])
+        chunk_data: memoryview = region_data[chunk_offset + 5:chunk_offset + 5 + chunk_size]
+        if chunk_compression == Compression.GZIP:
+            chunk_data = memoryview(gzip.decompress(chunk_data))
+        elif chunk_compression == Compression.ZLIB:
+            chunk_data = memoryview(zlib.decompress(chunk_data))
 
-        self.chunks[x][z] = nbt.deserialize(memoryview(decompressed_chunk_data))
+        self.chunks[x][z] = nbt.deserialize(chunk_data)
+        self.timestamps[x][z] = chunk_last_update
+        self.compression[x][z] = chunk_compression
 
-
-    def deserialize(self, region_data: memoryview, x: int, z: int):
+    def deserialize(self, region_data: memoryview):
         """ Find and deserialize all chunks stored in the region
 
         x & z here correspond to the location of the region as provided in the
         filename. Further down, x & z refer to the chunk offset.
         """
-        # Metadata is stored in two x-major matrixes. If both fields
-        # are zero, the chunk does not exist in the region (ungenerated).
-        for _z in range(0, 32):
-            for _x in range(0, 32):
-                self.deserialize_chunk(region_data, _x, _z)
+        # Metadata is stored in two x-major matrixes.
+        for z in range(0, 32):
+            for x in range(0, 32):
+                self.deserialize_chunk(region_data, x, z)
+
 
 def deserialize_file(filename: str) -> Region:
     with open(filename, 'rb') as f:
