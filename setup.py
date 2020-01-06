2bc388699902db3dcbc4703cdd7c445c957b6461
Seth Junot <xsetech@gmail.com>
2020-01-05 21:43:48 -0800

    Initial setuptools setup script content

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="aPyNBT",
+    version="0.3a",
+    packages=['aPyNBT'],
+    author="Seth Junot",
+    author_email="xsetech@gmail.com",
+    description="Deserializer and serializer for Minecraft binary formats",
+    long_description="A deserializer and serializer for Minecraft binary formats, specifically NBT and Region/Anvil.",
+    keywords="nbt region anvil minecraft python3",
+    url="https://github.com/xSetech/aPyNBT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+    ]
+)
